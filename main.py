18836b6607d5b519e11b13999a0ba5433e1a2773
Meiqiao <chenmeiqiao940926@gmail.com>
2022-06-15 18:05:21 +1200

    python script

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-import os
 import sys
+import os
 from github import Github
 from dotenv import load_dotenv
+
+load_dotenv()
+
+path = os.getenv("FILEPATH")
+username = os.getenv("USERNAME")
+password = os.getenv("PASSWORD")
+access_token = os.getenv("GITHUB_ACCESSTOKEN")
+
+
+def create():
+    user = Github(login_or_token=access_token, base_url="https://github.com").get_user()
+    repo = user.create_repo("test")
+
+
+if __name__ == "__main__":
+    create()
