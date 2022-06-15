3ad3c64ae9bf64e70719780d2dce4c4be1868858
Meiqiao <chenmeiqiao940926@gmail.com>
2022-06-16 10:09:38 +1200

    script done

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 path = os.getenv("FILEPATH")
-username = os.getenv("USERNAME")
-password = os.getenv("PASSWORD")
 access_token = os.getenv("GITHUB_ACCESSTOKEN")
 
+# Pass the dir name created throught command line as repo name
+reponame = sys.argv[1]
 
-def create():
-    user = Github(login_or_token=access_token, base_url="https://github.com").get_user()
-    repo = user.create_repo("test")
+
+def create(reponame):
+    user = Github(access_token).get_user()
+    # Create a remote repo
+    repo = user.create_repo(reponame)
+    print(f"Succesfully created repository {reponame}")
 
 
 if __name__ == "__main__":
-    create()
+    create(reponame)
--- myscript.sh
+++ myscript.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 function create(){
-  cd /home/meiqiao/test/
+  source .env
+  python main.py $DIR
+  cd $FOLDERPATH
   mkdir -p $DIR
   echo "creating $DIR"
   touch $DIR/README.md
   cd $DIR
   git init
   git add .
+  git commit -m "first commit"
+  git remote add origin git@github.com:$USERNAME/$DIR.git
+  git push -u origin master
 }
 read -p "What's your repo name:" DIR
 create $DIR
